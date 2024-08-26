7de8f92a61b5f77a801d611b02fdd7c6ffe3dd15
Sijia Ge <94318777+Konic-NLP@users.noreply.github.com>
2024-08-25 19:55:14 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, changeCount] = useState(0); // states and change the status of states
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    changeCount((c) => c + 1);
+  }
+  useEffect(function () {
+    //the function would be executed while logging the page
+    getAdvice();
+  }, []);
+  return (
+    <div>
+      <h1> {advice}</h1>
+      <button onClick={getAdvice}>Get advice </button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pirce of advice
+    </p>
+  );
+}
